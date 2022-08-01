ec72172097a7ab4ba3f9b1cabe6fd068381d8a3e
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-08-01 20:36:15 +0200

    footer

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <?php include 'navbar.php';?>
+  
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -139,7 +140,7 @@
   </div>
   <!-- End Contact -->
   <!-- Start Footer -->
-
+  <?php include 'footer.php';?>
 </body>
 
 </html>
