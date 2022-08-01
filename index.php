e1186c0b32e5e4c0f0886ae5a79bb4bf5d27a49a
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-08-01 20:32:26 +0200

    update

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include 'navbar.php';?>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Training Studio</title>
+  <link rel="stylesheet" href="css/index.css" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet" />
+</head>
+
+<body>
+
+ 
+  </header>
+  <!-- Start Header -->
+  <div class="header">
+    <video autoplay muted loop>
+      <source src="images/production ID_4550475.mp4" />
+    </video>
+    <div class="text-box">
+      <p>WORK HARDER, GET STRONGER</p>
+      <h1>EASY WITH OUR <span>Train Station</span></h1>
+      <a href="Signup.php">BECOME A MEMBER</a>
+    </div>
+  </div>
+  <!-- End Header -->
+
+
+  <!-- Start Classes -->
+  <div class="classes">
+    <div class="container">
+      <div class="special-heading">
+        <h2>OUR <span>CLASSES</span></h2>
+        <img src="images/line-dec.png" alt="" />
+        <p>
+          Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed
+          viverra<br />
+          ipsum dolor, ultricies fermentum massa consequat eu.
+        </p>
+      </div>
+      <div class="content">
+        <div class="classes-sec">
+          <div class="class activ">
+            <i class="fa fa-train" style="font-size:48px;color:rgb(60, 28, 238)"></i>
+
+            <h3>First Training Class</h3>
+          </div>
+          <div class="class">
+            <i class="fa fa-train" style="font-size:48px;color:rgb(60, 28, 238)"></i>
+
+            <h3>Second Training Class</h3>
+          </div>
+          <div class="class">
+            <i class="fa fa-train" style="font-size:48px;color:rgb(60, 28, 238)"></i>
+
+            <h3>Third Training Class</h3>
+          </div>
+          <div class="class">
+            <i class="fa fa-train" style="font-size:48px;color:rgb(60, 28, 238)"></i>
+
+            <h3>Fourth Training Class</h3>
+          </div>
+          <a href="book-a-ticket.html">View All Schedules</a>
+        </div>
+        <div class="desc-sec">
+          <img src="images/bullet.jpeg" alt="" />
+          <h3>First Training Class</h3>
+          <p>
+            Phasellus convallis mauris sed elementum vulputate. Donec posuere
+            leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed
+            vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia
+            gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut,
+            accumsan diam.
+          </p>
+          <a href="book-a-ticket.php">VIEW SCHEDULE</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Classes -->
+  <!-- Start Schedule -->
+  <!-- <div class="schedule">
+    <div class="container">
+      <div class="special-heading diff-heading">
+
+        <img src="images/line-dec.png" alt="" />
+
+      </div>
+
+      <table>
+        <tr>
+          <td></td>
+          <td>10:00AM - 11:30AM</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>2:00PM - 3:30PM</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+  </div> -->
+  <!-- End Schedule -->
+  <!-- Start Contact -->
+  <div class="contact">
+    <div class="container">
+      <div class="text">
+        <h4><span>Feel free to contact us</span></h4>
+        <a href="cu.php">Contact us</a>
+      </div>
+    </div>
+  </div>
+  <!-- End Contact -->
+  <!-- Start Footer -->
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Training Studio</title>
+  <link rel="stylesheet" href="css/navbar.css" />
+
+</head>
+     <!-- Start Nav -->
+  <nav>
+    <div class="logo">
+      <a href="index.html">Train<span>Station</span></a>
+    </div>
+    <i class="fas fa-ellipsis-v" id="menuButton"></i>
+    <ul id="menu">
+      <li> <a href="index.html">Home</li></a>
+      <li><a href="book-a-ticket.php">Ticket</a>
+      <li><a href="cu.php">Contact us</a>
+      <li><a href="Signup.php">Signup</a>
+
+
+    </ul>
+  </nav>
+  <!-- End Nav -->
+</html>
