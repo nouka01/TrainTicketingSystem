fdec4197b7ddfbace324c33096a3585b7e944e56
nouka01 <96498521+nouka01@users.noreply.github.com>
2022-08-03 08:45:50 +0200

    Update cu.php

--- cu.php
+++ cu.php
@@ -6,7 +6,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <!-- Start Contact -->
-<?php include 'navbar.php';?>
+<?php session_start();
+include 'navbar.php';?>
     </div>
     <div class="contact">
         <div class="container">
