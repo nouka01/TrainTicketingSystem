2bdbe31bc472003f10a1fdc1dd37f8dccdedffb4
Hazem Abou El-Fadl <hazem1913141@miuegypt.edu.eg>
2022-08-08 03:33:39 +0200

    Fixing CSS misbehaving on different browsers issue

--- train-ticket/train-ticket.php
+++ train-ticket/train-ticket.php
@@ -17,62 +17,66 @@
         <div class="left-ticket-part">
             <div class="passenger-name">
                 <label for="name">NAME OF PASSENGER:</label>
-                <br>
-                <input type="text" id="name" value="Dummy name" name="name" maxlength="20" minlength="2">
+    
+                <input type="text" id="name" value="Dummy name" name="name" maxlength="20" minlength="2" disabled>
             </div>
-            <br>
+
             <div class="from">
                 <label for="from">FROM:</label>
-                <br>
-                <input type="text" id="from" value="Dummy Source station" name="from" maxlength="20" minlength="2">
+    
+                <input type="text" id="from" value="Dummy Source station" name="from" maxlength="20" minlength="2" disabled>
             </div>
-            <br>
+
             <div class="to">
                 <label for="to">TO:</label>
-                <br>
-                <input type="text" id="to" value="Dummy destination station" name="to" maxlength="20" minlength="2">
+    
+                <input type="text" id="to" value="Dummy destination station" name="to" maxlength="20" minlength="2" disabled>
             </div>
-            <br>
+
             <div class="train-date">
                 <label for="date">DATE:</label>
-                <br>
-                <input type="date" id="date" name="date" maxlength="20" minlength="2">
+    
+                <input type="date" id="date" name="date" maxlength="20" minlength="2" disabled>
             </div>
         </div>
-        <br>
 
         <div class="right-ticket-part">
             <div class="train-name">
                 <label for="t-name">TRAIN:</label>
-                <br>
-                <input type="text" id="train-name" name="train-name" maxlength="20" minlength="2" value="Dummy train">
+    
+                <input type="text" id="train-name" name="train-name" maxlength="20" minlength="2" value="Dummy train" disabled>
             </div>
-            <br>
+
             <div class="platform-number">
                 <label for="platfrom">PLATFROM:</label>
-                <br>
-                <input type="text" id="platfrom" name="platfrom" maxlength="20" minlength="2" value="Dummy platfrom">
+    
+                <input type="text" id="platfrom" name="platfrom" maxlength="20" minlength="2" value="Dummy platfrom" disabled>
             </div>
-            <br>
+
             <div class="carriage-number">
                 <label for="carriage">CARRIAGE &numero;:</label>
-                <br>
-                <input type="number" id="carriage" name="carriage-number" maxlength="20" minlength="2" value="101213">
+    
+                <input type="number" id="carriage" name="carriage-number" maxlength="20" minlength="2" value="101213" disabled>
             </div>
+
+            <div class="filler-div">
+    
+                <input type="text" disabled>
+            </div>
+
         </div>
-        <br>
 
         <div class="middle-ticket-part">
             <div class="depart-time">
                 <label for="depart">DEPART TIME:</label>
-                <br>
-                <input type="time" id="depart" name="depart" maxlength="20" minlength="2">
+    
+                <input type="time" id="depart" name="depart" maxlength="20" minlength="2" disabled>
             </div>
 
             <div class="class-degree">
                 <label for="class">CLASS:</label>
-                <br>
-                <input type="text" id="class" value="x-Class" name="class" maxlength="20" minlength="2">
+    
+                <input type="text" id="class" value="x-Class" name="class" maxlength="20" minlength="2" disabled>
             </div>
         </div>
     </div>
