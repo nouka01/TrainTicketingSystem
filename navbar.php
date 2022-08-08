c55a9aff5dc0b917c54376835051dadead326549
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-08-08 13:44:16 +0200

    nnnn

--- navbar.php
+++ navbar.php
@@ -9,7 +9,7 @@
   <title>Training Studio</title>
   <link rel="stylesheet" href="css/navbar.css" />
   <link rel="stylesheet" href="css/index.css" />
-  <link rel="stylesheet" href="css/style.css" />
+  
   
 
 </head>
