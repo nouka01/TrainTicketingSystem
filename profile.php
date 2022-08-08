b0c0efb2b462645d4c6bcd93326cf6d54b5c1a3b
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-08-08 13:45:47 +0200

    demo for my profile page bootstrap css

---
+++ profile.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+   
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="container">
+    <div class="main-body">
+    
+          
+        
+    
+<div class="row gutters-sm">
+<div class="col-md-4 mb-3">
+<div class="card">
+<div class="card-body">
+<div class="d-flex flex-column align-items-center text-center">
+<img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+<div class="mt-3">
+<h4>John Doe</h4>
+<p class="text-secondary mb-1">Full Stack Developer</p>
+<p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+
+
+<button class="btn btn-primary">Upload photo</button>
+<br>
+<br>
+       
+<button class="btn btn-outline-primary">Remove photo</button>
+</div>
+</div>
+</div>
+</div>
+<div class="card mt-3">
+          
+              
+                  
+</div>
+</div>
+<div class="col-md-8">
+<div class="card mb-3">
+<div class="card-body">
+
+
+<div class="row">
+<div class="col-sm-3">
+<h6>Full Name</h6>
+</div>
+<div class="col-sm-9 text-secondary">
+<input type="text" id="fname" name="fname"><br>
+</div>
+</div>
+<hr>
+<div class="row">
+<div class="col-sm-3">
+<h6 >Email</h6>
+</div>
+<div class="col-sm-9 text-secondary"> <input type="email" id="mail" name="email"><br>
+</div>
+</div>
+<hr>
+<div class="row">
+<div class="col-sm-3">
+<h6 >Phone</h6>
+</div>
+<div class="col-sm-9 text-secondary">
+  
+<input type="text" id="pho" name="Phone"><br>
+</div>
+</div>
+<hr>
+<div class="row">
+<div class="col-sm-3">
+<h6>Mobile</h6>
+</div>
+<div class="col-sm-9 text-secondary">
+   
+<input type="text" id="mob" name="mobile"><br>
+</div>
+</div>
+<hr>
+<div class="row">
+<div class="col-sm-3">
+<h6 >Address</h6>
+</div>
+
+<input type="text" id="add" name="address"><br>
+</div>
+</div>
+ <hr>
+
+<a class="btn btn-info " target="__blank" href="">update</a>
+                    
+                
+
+     
+
+<style type="text/css">
+body{
+    margin-top:20px;
+    color: #1a202c;
+    text-align: left;
+    background-color: #e2e8f0;    
+}
+.main-body {
+    padding: 15px;
+}
+
+
+
+
+
+
+
+
+
+
+</style>
+
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
