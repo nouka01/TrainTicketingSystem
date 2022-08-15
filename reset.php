770e752258bebc5e3640705874beae3975ff151a
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-08-15 16:13:35 +0200

    reset password page

---
+++ reset.php
@@ -0,0 +1,44 @@
+
+<html>
+<head>
+<?php include 'navbar.php';?>
+	<meta charset="utf-8">
+	<title>Reset</title>
+
+	   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<section class="contact" id="contact">
+<br>
+    <div class="row">
+        <form action="index.html" method="post">
+        	<center><h1 class="heading"> <span>Reset</span> Password</h1></center>
+            <div class="inputBox">
+                <input type="password" placeholder="Old Password">
+            </div>
+            <br>
+            <div class="inputBox">
+                <input type="password" placeholder="New Password">
+            </div>
+            <div class="inputBox">
+                <input type="password" placeholder="Confirm Password">
+            </div>
+           
+            <center><a href="" class="btn">reset</a></center>
+            <br>
+            <br>
+            <br>
+        </form>
+    </div>
+    
+</section>
+<?php include 'footer.php';?>
+</html>
