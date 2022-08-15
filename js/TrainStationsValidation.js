eb30a6ce3fd09546786c938527e7696267be96fc
Adhamnasser <100451392+Adhamnasser@users.noreply.github.com>
2022-08-15 02:53:01 +0200

    Add files via upload

---
+++ js/TrainStationsValidation.js
@@ -0,0 +1,17 @@
+const station = document.getElementById('station-name')
+const form = document.getElementById('myForm')
+const errorelement=document.getElementById('error')
+form.addEventListener('submit',(e) =>{
+
+let messages=[]
+if(station.value===''|| station.value==null){
+    messages.push('name is required')
+
+}
+if(messages.length>0){
+
+    e.preventDefault()
+    errorelement.innerText=messages.join(',')
+}
+
+})
